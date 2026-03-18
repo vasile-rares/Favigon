fbc84dea77f3a5baaab309f4ef8fdcf9155c1b93
Vasile Rares-Mihail <75566187+vasile-rares@users.noreply.github.com>
2026-03-18 19:46:42 +0200

    refactor: Environment configuration and enhance Docker setup for improved variable handling

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: '/api',
-  githubClientId: 'Ov23libRfM2nwLH9x7iZ',
-  googleClientId: '210550939648-s30omgdg7k1mn7r9imboltu16ks9bjjn.apps.googleusercontent.com',
+  production: false,
+  apiBaseUrl: 'http://localhost:5207/api',
+  githubClientId: '',
+  googleClientId: '',
 };
